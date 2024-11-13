d5a22d2a104d224179ccc4b46e5c504603cadcc8
lenabenninger <lena.benninger@studentfr.ch>
2024-11-13 17:13:28 +0100

    creation function slide-in

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
 // gsap.from(".background-leaves", {
 //   scrollTrigger: {
 //     trigger: ".background-leaves",
@@ -11,37 +13,13 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 //   duration: 1.5,
 // });
 
-// function createScrollAnimation(target, trigger) {
-//   gsap.from(target, {
-//     scrollTrigger: {
-//       trigger: trigger,
-//       start: "center 90%",
-//       // end: "center 10%",
-//     },
-//     x: -1800,
-//     duration: 1.5,
-//   });
-// }
-
-// Call the function for each element you want to animate
-createScrollAnimation(".background-leaves", ".background-leaves");
-createScrollAnimation(".another-element", ".another-trigger");
-
-gsap.to(".floating-img", {
-  yPercent: -17,
-  scrollTrigger: {
-    trigger: ".",
-    scrub: 1,
-  },
-}); //parallaxe subtile sur images détourées
-
 gsap.to(".l-horizontal-content", {
   scrollTrigger: {
     trigger: ".l-horizontal",
     pin: true,
     anticipatePin: 1,
     scrub: 1,
-    end: "+=5000",
+    end: "+=6000",
   },
   x: "-82%",
   ease: "ease-in",
@@ -53,8 +31,44 @@ gsap.to(".l-end-content", {
     pin: true,
     anticipatePin: 1,
     scrub: 1,
-    end: "+=5000",
+    end: "+=2000",
   },
   x: "-50%",
   ease: "ease-in",
 });
+
+// Function to create the animation with different triggers and targets
+function slideInLeft(target, trigger) {
+  gsap.from(target, {
+    scrollTrigger: {
+      trigger: trigger,
+      start: "center 90%",
+      // end: "center 10%",
+    },
+    x: -1800,
+    duration: 1.5,
+  });
+}
+
+function slideInRight(target, trigger) {
+  gsap.from(target, {
+    scrollTrigger: {
+      trigger: trigger,
+      start: "center 90%",
+      // end: "center 10%",
+    },
+    x: 1800,
+    duration: 1.5,
+  });
+}
+
+// Call the function for each element you want to animate
+slideInLeft(".background-leaves", ".background-leaves");
+slideInLeft(".background-praying", ".background-praying");
+slideInLeft(".background-ground", ".background-ground");
+slideInLeft(".floating-butterfly", ".floating-butterfly");
+
+slideInRight(".background-sky", ".background-sky");
+slideInRight(".background-cemetery", ".background-cemetery");
+slideInRight(".background-earthquake", ".background-earthquake");
+slideInRight(".floating-unconscious", ".floating-unconscious");
