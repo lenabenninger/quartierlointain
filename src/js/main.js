2da32acdd8b4409adae708d7a03fa734cfbc2344
lenabenninger <lena.benninger@studentfr.ch>
2024-11-13 11:50:18 +0100

    parallax au scroll vertical

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,27 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".floating-img", {
+  yPercent: -17,
+  scrollTrigger: {
+    trigger: ".container",
+    scrub: 1,
+  },
+}); //parallaxe subtile sur images détourées
+
+// const viewportWidth = window.screen.width;
+// const sliderWidth = document.querySelector(
+//   ".l-horizontal-content img"
+// ).offsetWidth;
+
 gsap.to(".l-horizontal-content", {
   scrollTrigger: {
     trigger: ".l-horizontal",
     pin: true,
     anticipatePin: 1,
     scrub: true,
-    // end: "+=3000",
-    markers: false,
+    end: "+=3000",
   },
   x: "-85%",
-  ease: "none",
+  ease: "ease-in",
 });
