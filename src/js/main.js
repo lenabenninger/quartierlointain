c5bff84423dca65d7fc3fdc4110b9dbd3e90e86a
lenabenninger <lena.benninger@studentfr.ch>
2024-11-14 09:45:26 +0100

    slide-in images + textAppear

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,16 @@ gsap.registerPlugin(ScrollTrigger);
 //   duration: 1.5,
 // });
 
+gsap.to(".floating-img", {
+  yPercent: -40,
+  scrollTrigger: {
+    trigger: ".container",
+    scrub: 1,
+    // anticipatePin: 1,
+  },
+  ease: "none",
+}); //parallaxe subtile sur images détourées
+
 gsap.to(".l-horizontal-content", {
   scrollTrigger: {
     trigger: ".l-horizontal",
@@ -31,44 +41,76 @@ gsap.to(".l-end-content", {
     pin: true,
     anticipatePin: 1,
     scrub: 1,
-    end: "+=2000",
+    end: "+=1000",
   },
   x: "-50%",
-  ease: "ease-in",
+  ease: "ease-in-out",
 });
 
 // Function to create the animation with different triggers and targets
-function slideInLeft(target, trigger) {
+
+function slideInLeft(target) {
   gsap.from(target, {
     scrollTrigger: {
-      trigger: trigger,
-      start: "center 90%",
+      trigger: target,
+      start: "center 99%",
       // end: "center 10%",
     },
-    x: -1800,
-    duration: 1.5,
+    x: -300,
+    opacity: 0,
+    duration: 1,
+    ease: "ease-in-out",
   });
 }
 
-function slideInRight(target, trigger) {
+slideInLeft(".background-leaves");
+slideInLeft(".background-praying");
+slideInLeft(".background-ground");
+slideInLeft(".floating-butterfly");
+slideInLeft(".floating-graves");
+slideInLeft(".floating-fainting");
+
+function slideInRight(target) {
   gsap.from(target, {
     scrollTrigger: {
-      trigger: trigger,
+      trigger: target,
+      start: "center 97%",
+      // end: "center 10%",
+    },
+    x: 200,
+    opacity: 0,
+    duration: 1,
+    ease: "ease-in-out",
+  });
+}
+
+slideInRight(".background-sky");
+slideInRight(".background-cemetery");
+slideInRight(".background-earthquake");
+slideInRight(".floating-temple");
+slideInRight(".floating-praying");
+slideInRight(".floating-unconscious");
+slideInRight(".floating-hands");
+
+function textAppear(target) {
+  gsap.from(target, {
+    scrollTrigger: {
+      trigger: target,
       start: "center 90%",
       // end: "center 10%",
     },
-    x: 1800,
-    duration: 1.5,
+    opacity: 0,
+    transform: "scale(0.95)",
+    duration: 2,
+    ease: "ease-in-out",
   });
 }
+
+textAppear(".monologue-college");
+textAppear(".monologue-temple");
+textAppear(".monologue-tombe");
+textAppear(".monologue-maman");
 
 // Call the function for each element you want to animate
-slideInLeft(".background-leaves", ".background-leaves");
-slideInLeft(".background-praying", ".background-praying");
-slideInLeft(".background-ground", ".background-ground");
-slideInLeft(".floating-butterfly", ".floating-butterfly");
 
-slideInRight(".background-sky", ".background-sky");
-slideInRight(".background-cemetery", ".background-cemetery");
-slideInRight(".background-earthquake", ".background-earthquake");
-slideInRight(".floating-unconscious", ".floating-unconscious");
+textAppear();
